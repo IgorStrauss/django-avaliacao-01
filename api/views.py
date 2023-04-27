c96245861b7b761cfc0e0a48d1e8ba19b7b4e096
igorsenna <engenheirostrauss2@gmail.com>
2023-04-27 20:52:41 -0300

    Class PersonCarViewSet para listar person e todos os cars vinculados ao seu person_id

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.decorators import action
 
 from api.serializers import CarSerializer, PersonSerializer
 from management_cars_city.models import Car, Person
@@ -7,6 +8,10 @@ from management_cars_city.models import Car, Person
 class ListPerson(generics.ListAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+
+    @action(detail=True, methods=['get'], url_name='listar_persons')
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class ListCar(generics.ListAPIView):
